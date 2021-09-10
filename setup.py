a76f38386a2b5e9a584f131056a5a866c816562b
Vincent Davis <vincentmdavis@protonmail.com>
2021-09-10 14:49:42 -0400

    word

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='ln_graph_utils',
     version='1.0',
-    packages=find_packages(include=['graph_utils']),
+    packages=find_packages(include=['ln_graph_utils']),
     description='Common LN Graph Utils I use.',
     author='davis7',
     install_requires=["networkx"],
