29b4b7d8a0bfa690a297baf0ab4ca947a84a9028
Vincent Davis <vincentmdavis@protonmail.com>
2021-09-24 10:33:32 -0400

    added capacity attribute

--- ln_graph_utils/ln_graph_utils.py
+++ ln_graph_utils/ln_graph_utils.py
@@ -202,8 +202,9 @@ def get_channels_with_attrs(edges: List[Dict]) -> List[Tuple[str, str, Dict]]:
     channels = []
     for edge in edges:
         n1_weight, n2_weight = channel_fee_function(edge)
-        edge1 = (edge['node1_pub'], edge['node2_pub'], {"cost": n1_weight})
-        edge2 = (edge['node2_pub'], edge['node1_pub'], {"cost": n2_weight})
+        capacity = int(edge['capacity'])
+        edge1 = (edge['node1_pub'], edge['node2_pub'], {"cost": n1_weight, "capacity": capacity})
+        edge2 = (edge['node2_pub'], edge['node1_pub'], {"cost": n2_weight, "capacity": capacity})
         channels.extend([edge1, edge2])
     return channels
 
